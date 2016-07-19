595453e55d6fc6a44c64ef19cd4130c1027be4d7
Dibyalochan Nayak <dibyalochan@ewaycorp.com>
2016-07-19 13:11:48 +0000

    created models and migrations

---
+++ database/migrations/2016_07_19_122921_create_hotels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('hotel_id');            
+            $table->string('comments');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            Schema::dropIfExists('comments');
+        });
+    }
+}
